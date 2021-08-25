0599e5883f68ab20e654ecb64fa570d5f78e0414
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-24 20:25:21 -0600

    Connected to database

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -12,7 +15,15 @@ app.set("view", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
 
 app.use("/", indexRouter);
 
-app.listen(process.env.PORT || 9000);
+const PORT = process.env.PORT || 9000;
+
+mongoose.connection.once("open", () => console.log("Connected to Mongoose..."));
+mongoose.connect(process.env.DATABASE_URL)
+	.then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+	.catch((error) => console.log(error));
