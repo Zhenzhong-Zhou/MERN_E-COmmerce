bdd2e6027c05b6b240156eb0dff1d4d21abbc234
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-25 21:50:03 -0600

    Remove require from mproduct model

--- server/models/product.js
+++ server/models/product.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    creator: {type: String, required: true},
-    name: {type: String, required: true},
-    price: {type: Number, required: true},
+    title: String,
+    description: String,
+    creator: String,
+    name: String,
+    price: Number,
     tags: [String],
-    selectedFile: {type: String, required: true},
+    selectedFile: String,
     likeCount: {
         type: Number,
         default: 0
